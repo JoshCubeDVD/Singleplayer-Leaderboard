aaab3b8bec4c30c6b7be287a4371fe8a7f30cf30
JoshuaR <doodlebugboo03@gmail.com>
2026-07-18 17:02:56 -0400

    Set up the player table

--- leaderboard.sql
+++ leaderboard.sql
@@ -1,7 +1,13 @@
 CREATE DATABASE SPLeaderboardDB;
 
-USE SPLeaderboardDB
+USE SPLeaderboardDB;
 
 -- Sets up the players table
 
-CREATE TABLE Player 
+CREATE TABLE Player (
+  PlayerID INT AUTO_INCREMENT PRIMARY KEY,
+  PlayerName VARCHAR(50) NOT NULL,
+  Points INT DEFAULT 0,
+  GamesPlayed INT DEFAULT 0,
+  
+);
